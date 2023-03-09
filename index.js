060af6ed2823245ec4f13472af5e3ee6433c9f54
razeeth11 <razeeth011@gmail.com>
2023-03-09 22:21:51 +0530

    Data

---
+++ index.js
@@ -0,0 +1,247 @@
+const zen_class_users = [
+	{
+      "name": "John Doe",
+		"email": "johndoe@example.com",
+		"phone_number": "1234567890",
+		"dob": "01/01/1980",
+		"gender": "Male",
+		"address": "123 Main Street, Anytown, Anystate 12345",
+    "codekata" : 185,
+    "attendance" : '85%'
+	},
+	{
+		"name": "Jane max",
+		"email": "janemax@example.com",
+		"phone_number": "0987654321",
+		"dob": "02/02/1981",
+		"gender": "Female",
+		"address": "456 Park Avenue, Anytown, Anystate 54321",
+    "codekata" : 125,
+    "attendance" : '87%'
+	},
+	{
+		"name": "John Smith",
+		"email": "johnsmith@example.com",
+		"phone_number": "1357924680",
+		"dob": "03/03/1982",
+		"gender": "Male",
+		"address": "789 Washington Street, Anytown, Anystate 98765",
+    "codekata" : 69,
+    "attendance" : '93%'
+	},
+	{
+		"name": "Jane adam",
+		"email": "janeadam@example.com",
+		"phone_number": "2468024680",
+		"dob": "04/04/1983",
+		"gender": "Female",
+		"address": "123 Ocean Avenue, Anytown, Anystate 88888",
+    "codekata" : 58,
+    "attendance" : '89%'
+	},
+	{
+		"name": "Richard Johnson",
+		"email": "richardjohnson@example.com",
+		"phone_number": "5555555555",
+		"dob": "05/05/1984",
+		"gender": "Male",
+		"address": "456 Maple Street, Anytown, Anystate 77777",
+    "codekata" : 180,
+    "attendance" : '78%'
+	},
+	{
+		"name": "Mary Johnson",
+		"email": "maryjohnson@example.com",
+		"phone_number": "6666666666",
+		"dob": "06/06/1985",
+		"gender": "Female",
+		"address": "789 Broadway Street, Anytown, Anystate 66666",
+    "codekata" : 168,
+    "attendance" : '98%'
+	},
+	{
+		"name": "William Anderson",
+		"email": "williamanderson@example.com",
+		"phone_number": "7777777777",
+		"dob": "07/07/1986",
+		"gender": "Male",
+		"address": "123 Park Place, Anytown, Anystate 55555",
+    "codekata" : 132,
+    "attendance" : '85%'
+	},
+	{
+		"name": "Karen Anderson",
+		"email": "karenanderson@example.com",
+		"phone_number": "8888888888",
+		"dob": "08/08/1987",
+		"gender": "Female",
+		"address": "456 5th Avenue, Anytown, Anystate 44444",
+    "codekata" : 158,
+    "attendance" : '95%'
+	},
+	{
+		"name": "Christopher Wilson",
+		"email": "christopherwilson@example.com",
+		"phone_number": "9999999999",
+		"dob": "09/09/1988",
+		"gender": "Male",
+		"address": "789 6th Avenue, Anytown, Anystate 33333",
+    "codekata" : 64,
+    "attendance" : '90%'
+	},
+	{
+		"name": "Amanda Wilson",
+		"email": "amandawilson@example.com",
+		"phone_number": "1010101010",
+		"dob": "10/10/1989",
+		"gender": "Female",
+		"address": "123 Main Street, Anytown, Anystate 22222",
+    "codekata" : 158,
+    "attendance" : '89%'
+	},
+	{
+		"name": "Matthew Moore",
+		"email": "matthewmoore@example.com",
+		"phone_number": "2020202020",
+		"dob": "11/11/1990",
+		"gender": "Male",
+		"address": "456 Park Avenue, Anytown, Anystate 11111",
+    "codekata" : 185,
+    "attendance" : '69%'
+	},
+	{
+		"name": "Jennifer Moore",
+		"email": "jennifermoore@example.com",
+		"phone_number": "3030303030",
+		"dob": "12/12/1991",
+		"gender": "Female",
+		"address": "789 Washington Street, Anytown, Anystate 00000",
+    "codekata" : 124,
+    "attendance" : '78%'
+	},
+	{
+		"name": "Daniel Taylor",
+		"email": "danieltaylor@example.com",
+		"phone_number": "3232323232",
+		"dob": "01/01/1992",
+		"gender": "Male",
+		"address": "123 Ocean Avenue, Anytown, Anystate 99999",
+    "codekata" : 86,
+    "attendance" : '84%'
+	},
+	{
+		"name": "Susan Taylor",
+		"email": "susantaylor@example.com",
+		"phone_number": "4242424242",
+		"dob": "02/02/1993",
+		"gender": "Female",
+		"address": "456 Maple Street, Anytown, Anystate 88888",
+    "codekata" : 189,
+    "attendance" : '86%'
+	},
+	{
+		"name": "David White",
+		"email": "davidwhite@example.com",
+		"phone_number": "5353535353",
+		"dob": "03/03/1994",
+		"gender": "Male",
+		"address": "789 Broadway Street, Anytown, Anystate 77777",
+    "codekata" : 201,
+    "attendance" : '59%'
+	},
+	{
+		"name": "Linda White",
+		"email": "lindawhite@example.com",
+		"phone_number": "6464646464",
+		"dob": "04/04/1995",
+		"gender": "Female",
+		"address": "123 Park Place, Anytown, Anystate 66666",
+    "codekata" : 94,
+    "attendance" : '76%'
+	},
+	{
+		"name": "Paul Harris",
+		"email": "paulharris@example.com",
+		"phone_number": "7575757575",
+		"dob": "05/05/1996",
+		"gender": "Male",
+		"address": "456 5th Avenue, Anytown, Anystate 55555",
+    "codekata" : 97,
+    "attendance" : '85%'
+	},
+	{
+		"name": "Kathy Harris",
+		"email": "kathyharris@example.com",
+		"phone_number": "8686868686",
+		"dob": "06/06/1997",
+		"gender": "Female",
+		"address": "789 6th Avenue, Anytown, Anystate 44444",
+    "codekata" : 198,
+    "attendance" : '79%'
+	}
+]
+
+const mentors = [
+  {
+    "name": "John Smith",
+    "no_of_mentees": 14,
+    "email": "johnsmith@example.com",
+    "gender": "male"
+  },
+  {
+    "name": "Jenny Jones",
+    "no_of_mentees": 16,
+    "email": "jennyjones@example.com",
+    "gender": "female"
+  },
+  {
+    "name": "Paul Anderson",
+    "no_of_mentees": 18,
+    "email": "paulanderson@example.com",
+    "gender": "male"
+  },
+  {
+    "name": "Emily Taylor",
+    "no_of_mentees": 17,
+    "email": "emilytaylor@example.com",
+    "gender": "female"
+  },
+  {
+    "name": "Sam Brown",
+    "no_of_mentees": 15,
+    "email": "sambrown@example.com",
+    "gender": "male"
+  },
+  {
+    "name": "Olivia Williams",
+    "no_of_mentees": 19,
+    "email": "oliviawilliams@example.com",
+    "gender": "female"
+  },
+  {
+    "name": "James Davis",
+    "no_of_mentees": 13,
+    "email": "jamesdavis@example.com",
+    "gender": "male"
+  },
+  {
+    "name": "Sophia Miller",
+    "no_of_mentees": 16,
+    "email": "sophiamiller@example.com",
+    "gender": "female"
+  },
+  {
+    "name": "Michael Wilson",
+    "no_of_mentees": 14,
+    "email": "michaelwilson@example.com",
+    "gender": "male"
+  },
+  {
+    "name": "Isabella Taylor",
+    "no_of_mentees": 18,
+    "email": "isabellataylor@example.com",
+    "gender": "female"
+  }
+]
+
+
