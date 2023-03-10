e1c0dc9065b27f12a35625e8d5842f1753035357
razeeth11 <razeeth011@gmail.com>
2023-03-10 10:34:46 +0530

    changes

--- router.js
+++ router.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import {
   users,
+  users_absent,
+  users_codekata,
   mentors,
+  october_1to15_data,
   october_data,
   november_data,
   december_data,
@@ -9,30 +12,87 @@ import {
 
 const router = express.Router()
 
+
+// ----------------------Zen Class Users Data ------------------------------
+
 router.get("/users", async function (request, response) {
   const users_data = await users();
 
   response.send(users_data);
 });
+
+
+// ----------------------Zen Class Users absent Data ------------------------------
+
+router.get("/users", async function (request, response) {
+  const users_absent_data = await users_absent();
+
+  response.send(users_absent_data);
+});
+
+
+// ----------------------Zen Class Users codekata Data ------------------------------
+
+router.get("/users", async function (request, response) {
+  const users_codekata_data = await users_codekata();
+
+  response.send(users_codekata_data);
+});
+
+
+// ----------------------Zen Class mentors Data ------------------------------
+
 router.get("/mentors", async function (request, response) {
   const mentors_data = await mentors();
 
   response.send(mentors_data);
 });
+
+
+// ----------------------Zen Class mentor mentees Data ------------------------------
+
+router.get("/mentors", async function (request, response) {
+  const mentors_mentees = await mentors_mentees_data();
+
+  response.send(mentors_mentees);
+});
+
+
+// ----------------------Zen Class october Data ------------------------------
+
 router.get("/october", async function (request, response) {
   const october = await october_data();
 
   response.send(october);
 });
+
+
+// ----------------------Zen Class october 1 to 15 Data ------------------------------
+
+router.get("/october", async function (request, response) {
+  const october1_15 = await october_1to15_data();
+
+  response.send(october1_15);
+});
+
+
+// ----------------------Zen Class november Data ------------------------------
+
 router.get("/november", async function (request, response) {
   const november = await november_data();
 
   response.send(november);
 });
+
+
+// ----------------------Zen Class december Data ------------------------------
+
 router.get("/december", async function (request, response) {
   const december = await december_data();
 
   response.send(december);
 });
+
+
 
 export default router;
--- services.js
+++ services.js
@@ -1,8 +1,32 @@
 import { client } from "./index.js";
 
+
 export async function users() {
-  return await client.db("Zen_Class_DB").collection("users").find({}).toArray();
+  return await client
+  .db("Zen_Class_DB")
+  .collection("users")
+  .find({})
+  .toArray();
 }
+
+
+export async function users_codekata() {
+  return await client
+  .db("Zen_Class_DB")
+  .collection("users")
+  .find({},{name:1,codekata:1,_id:0})
+  .toArray();
+}
+
+
+export async function users_absent() {
+  return await client
+  .db("Zen_Class_DB")
+  .collection("OCT_DATA")
+  .find({day : {$gte : 15 , $lte : 31}},{date:1,absentees : 1,task:1,_id:0})
+  .toArray();
+}
+
 
 export async function mentors() {
   return await client
@@ -12,6 +36,16 @@ export async function mentors() {
     .toArray();
 }
 
+
+export async function mentors_mentees_data() {
+  return await client
+    .db("Zen_Class_DB")
+    .collection("mentors")
+    .find({mentees: {$gte : 15}},{name:1,mentees:1,_id:0})
+    .toArray();
+}
+
+
 export async function october_data() {
   return await client
     .db("Zen_Class_DB")
@@ -20,6 +54,17 @@ export async function october_data() {
     .toArray();
 }
 
+
+export async function october_1to15_data() {
+  return await client
+    .db("Zen_Class_DB")
+    .collection("OCT_DATA")
+    .find({ day: { $gte: 1, $lte: 15 } }, { date:1, task: 1, topic: 1, _id:0 })
+    .toArray();
+}
+
+
+
 export async function november_data() {
   return await client
     .db("Zen_Class_DB")
@@ -27,6 +72,7 @@ export async function november_data() {
     .find({})
     .toArray();
 }
+
 
 export async function december_data() {
   return await client
